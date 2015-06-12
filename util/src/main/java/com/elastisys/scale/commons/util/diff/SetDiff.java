dd3f477a4a696358d4a2b59b19baf65d15e07529
petergardfjall <peter.gardfjall.work@gmail.com>
2015-06-12 10:34:14 +0200

    added SetDiff to commons.util

---
+++ util/src/main/java/com/elastisys/scale/commons/util/diff/SetDiff.java
@@ -0,0 +1,74 @@
+package com.elastisys.scale.commons.util.diff;
+
+import java.util.Set;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+
+/**
+ * Compares two sets of objects, a source set and a destination set, and
+ * determines which objects need to be added and removed to go from the source
+ * set to the destination set.
+ *
+ *
+ */
+public class SetDiff<T> {
+
+	private final Set<T> sourceSet;
+	private final Set<T> destinationSet;
+
+	/**
+	 * Create a {@link SetDiff}, that determines how to get from a source set of
+	 * objects to a destination set.
+	 *
+	 * @param sourceSet
+	 *            The source set. Can be <code>null</code>, which counts as the
+	 *            empty set.
+	 * @param destinationSet
+	 *            The destination set. Can be <code>null</code>, which counts as
+	 *            the empty set.
+	 */
+	public SetDiff(Set<T> sourceSet, Set<T> destinationSet) {
+		ImmutableSet<T> emptySet = ImmutableSet.of();
+		this.sourceSet = Optional.fromNullable(sourceSet).or(emptySet);
+		this.destinationSet = Optional.fromNullable(destinationSet)
+				.or(emptySet);
+	}
+
+	/**
+	 * Determines which objects need to be added to go from {@code sourceSet} to
+	 * {@code destinationSet}.
+	 *
+	 * @return
+	 */
+	public Set<T> added() {
+		return Sets.difference(this.destinationSet, this.sourceSet)
+				.immutableCopy();
+
+	}
+
+	/**
+	 * Determines which objects need to be removed to go from {@code sourceSet}
+	 * to {@code destinationSet}.
+	 *
+	 * @return
+	 */
+	public Set<T> removed() {
+		return Sets.difference(this.sourceSet, this.destinationSet)
+				.immutableCopy();
+	}
+
+	/**
+	 * Determines which objects are common to both {@code sourceSet} and
+	 * {@code destinationSet}. That is, all objects that are in both
+	 * {@code sourceSet} and {@code destinationSet}.
+	 *
+	 * @return
+	 */
+	public Set<T> common() {
+		return Sets.intersection(this.sourceSet, this.destinationSet)
+				.immutableCopy();
+	}
+
+}
