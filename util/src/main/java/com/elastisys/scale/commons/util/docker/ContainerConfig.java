15117f5f491436672796350d7844de919421c471
petergardfjall <peter.gardfjall.work@gmail.com>
2015-12-04 15:33:44 +0100

    added some simple utility classes for running Docker via command-line tool

---
+++ util/src/main/java/com/elastisys/scale/commons/util/docker/ContainerConfig.java
@@ -0,0 +1,151 @@
+package com.elastisys.scale.commons.util.docker;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.base.MoreObjects;
+
+/**
+ * Describes a {@link Docker} container to be launched.
+ *
+ * @see Docker
+ */
+public class ContainerConfig {
+
+	/** Docker image of form: {@code IMAGE[:TAG|@DIGEST]} */
+	private final String image;
+
+	/** Port mappings of form {@code [<hostport>:]<containerport>}. */
+	private final List<String> portMappings;
+
+	/** Environment variables. */
+	private final Map<String, String> env;
+
+	private ContainerConfig(String image, List<String> portMappings,
+			Map<String, String> env) {
+		this.image = image;
+		this.portMappings = portMappings;
+		this.env = env;
+	}
+
+	/**
+	 * Docker image of form: {@code IMAGE[:TAG|@DIGEST]}
+	 *
+	 * @return
+	 */
+	public String getImage() {
+		return this.image;
+	}
+
+	/**
+	 * Port mappings of form {@code [<hostport>:]<containerport>}.
+	 *
+	 * @return
+	 */
+	public List<String> getPortMappings() {
+		return this.portMappings;
+	}
+
+	/**
+	 * Environment variables.
+	 *
+	 * @return
+	 */
+	public Map<String, String> getEnv() {
+		return this.env;
+	}
+
+	/**
+	 * Converts this {@link ContainerConfig} to its corresponding
+	 * {@code docker run} command-line arguments.
+	 *
+	 * @return
+	 */
+	public List<String> toRunCommandArgs() {
+		List<String> cmdArgs = new ArrayList<>();
+		cmdArgs.addAll(asList("docker", "run", "-d"));
+		for (String portMapping : this.portMappings) {
+			cmdArgs.addAll(asList("-p", portMapping));
+		}
+		for (String envKey : this.env.keySet()) {
+			cmdArgs.addAll(asList("-e",
+					String.format("%s=%s", envKey, this.env.get(envKey))));
+		}
+		cmdArgs.add(this.image);
+		return cmdArgs;
+	}
+
+	/**
+	 * Creates a new {@link ContainerConfigBuilder}.
+	 *
+	 * @return
+	 */
+	public static ContainerConfigBuilder builder() {
+		return new ContainerConfigBuilder();
+	}
+
+	@Override
+	public String toString() {
+		return MoreObjects.toStringHelper(this).add("image", this.image)
+				.add("portMappings", this.portMappings).add("env", this.env)
+				.toString();
+	}
+
+	/**
+	 * Builder of {@link ContainerConfig}.
+	 */
+	public static class ContainerConfigBuilder {
+		/** Docker image of form: {@code IMAGE[:TAG|@DIGEST]} */
+		private String image;
+
+		/** Port mappings of form {@code [<hostport>:]<containerport>}. */
+		private List<String> portMappings = new ArrayList<>();
+
+		/** Environment variables. */
+		private Map<String, String> env = new HashMap<>();
+
+		public ContainerConfig build() {
+			checkArgument(this.image != null, "no docker image given");
+			return new ContainerConfig(this.image, this.portMappings, this.env);
+		}
+
+		/**
+		 * Sets a Docker image of form: {@code IMAGE[:TAG|@DIGEST]}
+		 *
+		 * @param image
+		 * @return
+		 */
+		public ContainerConfigBuilder image(String image) {
+			this.image = image;
+			return this;
+		}
+
+		/**
+		 * Adds a port mapping of form {@code [<hostport>:]<containerport>}.
+		 *
+		 * @param portMapping
+		 * @return
+		 */
+		public ContainerConfigBuilder portMapping(String portMapping) {
+			this.portMappings.add(portMapping);
+			return this;
+		}
+
+		/**
+		 * Adds an environment variable.
+		 *
+		 * @param key
+		 * @param value
+		 * @return
+		 */
+		public ContainerConfigBuilder env(String key, String value) {
+			this.env.put(key, value);
+			return this;
+		}
+	}
+}
---
+++ util/src/main/java/com/elastisys/scale/commons/util/docker/Docker.java
@@ -0,0 +1,152 @@
+package com.elastisys.scale.commons.util.docker;
+
+import static java.util.Arrays.asList;
+
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
+
+/**
+ * A simple class for managing Docker containers using the command-line
+ * interface.
+ * <p/>
+ * Note: this class assumes a working Docker installation and that the
+ * {@code docker} executable is on the system {@code PATH}.
+ */
+public class Docker {
+	private final static Logger LOG = LoggerFactory.getLogger(Docker.class);
+
+	/**
+	 * Runs a Docker container.
+	 *
+	 * @param config
+	 *            The {@link ContainerConfig}.
+	 * @return The container id.
+	 */
+	public static String run(ContainerConfig config) throws DockerException {
+		List<String> runCmd = config.toRunCommandArgs();
+		return execute(runCmd);
+	}
+
+	/**
+	 * Removes a Docker container.
+	 *
+	 * @param containerId
+	 *            The container id.
+	 * @param force
+	 *            Force removal of running container.
+	 * @throws DockerException
+	 */
+	public static void remove(String containerId, boolean force)
+			throws DockerException {
+		List<String> cmdArgs = new ArrayList<>();
+		cmdArgs.addAll(asList("docker", "rm"));
+		if (force) {
+			cmdArgs.add("-f");
+		}
+		cmdArgs.add(containerId);
+		execute(cmdArgs);
+	}
+
+	/**
+	 * Pulls an image from a Docker registry.
+	 *
+	 * @param image
+	 *            An image of form {@code NAME[:TAG|@DIGEST]}.
+	 * @throws DockerException
+	 */
+	public static void pull(String image) throws DockerException {
+		List<String> cmdArgs = asList("docker", "pull", image);
+		execute(cmdArgs);
+	}
+
+	/**
+	 * Checks if an image exists locally on this host.
+	 *
+	 * @param image
+	 *            An image of form {@code NAME[:TAG|@DIGEST]}.
+	 * @return <code>true</code> if the image exists locally, <code>false</code>
+	 *         otherwise.
+	 */
+	public static boolean isLocalImage(String image) {
+		List<String> cmdArgs = asList("docker", "image", "-q", image);
+		return !execute(cmdArgs).trim().isEmpty();
+	}
+
+	/**
+	 * Executes a command and returns any output. If the command execution
+	 * failed or returned a non-zero exit status a {@link DockerException} is
+	 * raised.
+	 *
+	 * @param runCmd
+	 * @return
+	 * @throws DockerException
+	 */
+	private static String execute(List<String> runCmd) throws DockerException {
+		LOG.debug("running: {}", Joiner.on(" ").join(runCmd));
+		StreamReader reader = null;
+		int exitStatus = 0;
+		try {
+			Process process = new ProcessBuilder(runCmd)
+					.redirectErrorStream(true).start();
+			reader = new StreamReader(process.getInputStream());
+			reader.start();
+			exitStatus = process.waitFor();
+		} catch (Exception e) {
+			String output = (reader != null) ? reader.getOutput()
+					: "no output was captured";
+			throw new DockerException(String.format(
+					"failed on docker run: {}\ncaptured output:{}", output), e);
+		}
+		if (exitStatus != 0) {
+			throw new DockerException(String.format(
+					"failed on docker run: {}\ncaptured output:{}",
+					reader.getOutput()));
+		}
+		return reader.getOutput().trim();
+	}
+
+	/**
+	 * A {@link Thread} that collects all characters read from an
+	 * {@link InputStream}. The output collected so far can be retrieved via
+	 * {@link #getOutput()}.
+	 */
+	private static class StreamReader extends Thread {
+		private final StringWriter buffer = new StringWriter();
+		private final InputStream stream;
+
+		public StreamReader(InputStream stream) {
+			super("reader");
+			setDaemon(true);
+			this.stream = stream;
+		}
+
+		@Override
+		public void run() {
+			try {
+				byte[] block = new byte[1024];
+				while (this.stream.read(block) > 0) {
+					this.buffer.write(new String(block, Charsets.UTF_8));
+				}
+			} catch (Exception e) {
+				throw new RuntimeException("read failed: " + e.getMessage(), e);
+			}
+		}
+
+		/**
+		 * Returns the input consumed thus far from the stream.
+		 *
+		 * @return
+		 */
+		public String getOutput() {
+			return this.buffer.toString();
+		}
+	}
+}
