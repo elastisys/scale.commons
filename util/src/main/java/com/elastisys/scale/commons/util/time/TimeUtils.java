7b18efbe9538d8ee8cef7060c530a1cd6977d76e
petergardfjall <peter.gardfjall.work@gmail.com>
2016-01-14 15:36:51 +0100

    added durationAsString

--- util/src/main/java/com/elastisys/scale/commons/util/time/TimeUtils.java
+++ util/src/main/java/com/elastisys/scale/commons/util/time/TimeUtils.java
@@ -1,11 +1,13 @@
 package com.elastisys.scale.commons.util.time;
 
 import org.joda.time.DateTime;
+import org.joda.time.Duration;
+import org.joda.time.Period;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
 
 /**
  * Convenience methods for working with time.
- * 
- * 
  */
 public class TimeUtils {
 
@@ -16,17 +18,40 @@ public class TimeUtils {
 	 * That is, the comparison is based solely on milliseconds, which allows
 	 * timestamps with different time zones to be correctly compared (which is
 	 * not the case with {@link DateTime#equals(Object)}).
-	 * 
+	 *
 	 * @param instant1
 	 *            First {@link DateTime} instance. May be <code>null</code>.
 	 * @param instant2
 	 *            Second {@link DateTime} instance. May be <code>null</code>.
 	 * @return <code>true</code> if the {@link DateTime} instances represent the
 	 *         same time instant.
-	 * 
+	 *
 	 */
 	public static boolean equal(DateTime instant1, DateTime instant2) {
 		return (instant1 == instant2)
 				|| ((instant1 != null) && instant1.isEqual(instant2));
+	}
+
+	/**
+	 * Returns a human-readable string representation of a {@link Duration},
+	 * which expresses the duration in hours, minutes and seconds using the
+	 * highest possible units.
+	 *
+	 * @param duration
+	 * @return A duration such as "48 hours, 10 minutes, 1 second"
+	 */
+	public static String durationAsString(Duration duration) {
+		if (duration.isShorterThan(new Duration(1))) {
+			return "0 seconds";
+		}
+
+		PeriodFormatter formatter = new PeriodFormatterBuilder()
+				.printZeroNever().appendHours().appendSuffix(" hour", " hours")
+				.appendSeparator(", ").printZeroNever().appendMinutes()
+				.appendSuffix(" minute", " minutes").appendSeparator(", ")
+				.printZeroNever().appendSeconds()
+				.appendSuffix(" second", " seconds").toFormatter();
+		new Period(duration.getStandardDays());
+		return formatter.print(duration.toPeriod());
 	}
 }
--- util/src/test/java/com/elastisys/scale/commons/util/time/TestTimeUtils.java
+++ util/src/test/java/com/elastisys/scale/commons/util/time/TestTimeUtils.java
@@ -1,15 +1,18 @@
 package com.elastisys.scale.commons.util.time;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import org.joda.time.DateTime;
+import org.joda.time.Duration;
 import org.junit.Test;
 
 /**
  * Verify behavior of {@link TimeUtils} class.
- * 
- * 
+ *
+ *
  */
 public class TestTimeUtils {
 
@@ -30,5 +33,37 @@ public class TestTimeUtils {
 
 		assertFalse(TimeUtils.equal(null, utcInstant));
 		assertFalse(TimeUtils.equal(utcInstant, null));
+	}
+
+	@Test
+	public void testDurationAsString() {
+		Duration twoDays = new Duration(2 * 24 * 3600 * 1000);
+		Duration oneDayPlus = new Duration(
+				1 * 24 * 3600 * 1000 + 60 * 55 * 1000 + 10 * 1000);
+		Duration oneDay = new Duration(1 * 24 * 3600 * 1000);
+		Duration hours = new Duration(
+				23 * 3600 * 1000 + 1800 * 1000 + 1 * 1000);
+		Duration hour = new Duration(1 * 3600 * 1000);
+		Duration minutes = new Duration(55 * 60 * 1000 + 30 * 1000);
+		Duration minute = new Duration(1 * 60 * 1000);
+		Duration seconds = new Duration(55 * 1000);
+		Duration second = new Duration(1 * 1000);
+		Duration zero = new Duration(0);
+		Duration negative = new Duration(-100);
+
+		assertThat(TimeUtils.durationAsString(twoDays), is("48 hours"));
+		assertThat(TimeUtils.durationAsString(oneDayPlus),
+				is("24 hours, 55 minutes, 10 seconds"));
+		assertThat(TimeUtils.durationAsString(oneDay), is("24 hours"));
+		assertThat(TimeUtils.durationAsString(hours),
+				is("23 hours, 30 minutes, 1 second"));
+		assertThat(TimeUtils.durationAsString(hour), is("1 hour"));
+		assertThat(TimeUtils.durationAsString(minutes),
+				is("55 minutes, 30 seconds"));
+		assertThat(TimeUtils.durationAsString(minute), is("1 minute"));
+		assertThat(TimeUtils.durationAsString(seconds), is("55 seconds"));
+		assertThat(TimeUtils.durationAsString(second), is("1 second"));
+		assertThat(TimeUtils.durationAsString(zero), is("0 seconds"));
+		assertThat(TimeUtils.durationAsString(negative), is("0 seconds"));
 	}
 }
