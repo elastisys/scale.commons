67a7a3240cde87f662a9d7a656478379d6285064
petergardfjall <peter.gardfjall.work@gmail.com>
2016-09-12 14:39:00 +0200

    added commons.openstack module for common openstack client initialization code

---
+++ openstack/src/main/java/com/elastisys/scale/commons/openstack/ApiAccessConfig.java
@@ -0,0 +1,171 @@
+package com.elastisys.scale.commons.openstack;
+
+import static com.google.common.base.Objects.equal;
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Optional;
+
+/**
+ * Configuration object for an {@link OSClientFactory}, which declares how to
+ * authenticate and what OpenStack region to operate against.
+ * <p/>
+ * The {@link OSClientFactory} can be configured to use either use version 2 or
+ * version 3 of the
+ * <a href="http://docs.openstack.org/developer/keystone/http-api.html#history"
+ * >identity HTTP API</a>.
+ */
+public class ApiAccessConfig {
+
+	/**
+	 * The default timeout in milliseconds until a connection is established.
+	 */
+	public static final int DEFAULT_CONNECTION_TIMEOUT = 10000;
+	/**
+	 * The default socket timeout ({@code SO_TIMEOUT}) in milliseconds, which is
+	 * the timeout for waiting for data or, put differently, a maximum period
+	 * inactivity between two consecutive data packets).
+	 */
+	public static final int DEFAULT_SOCKET_TIMEOUT = 10000;
+
+	/**
+	 * Declares how to authenticate with the OpenStack identity service
+	 * (Keystone).
+	 */
+	private final AuthConfig auth;
+
+	/**
+	 * The particular OpenStack region (out of the ones available in Keystone's
+	 * service catalog) to connect to. For example, {@code RegionOne}.
+	 */
+	private final String region;
+
+	/**
+	 * The timeout in milliseconds until a connection is established.
+	 */
+	private final Integer connectionTimeout;
+
+	/**
+	 * The socket timeout ({@code SO_TIMEOUT}) in milliseconds, which is the
+	 * timeout for waiting for data or, put differently, a maximum period
+	 * inactivity between two consecutive data packets.
+	 */
+	private final Integer socketTimeout;
+
+	/**
+	 * Creates a new {@link ApiAccessConfig}.
+	 *
+	 * @param auth
+	 *            Declares how to authenticate with the OpenStack identity
+	 *            service (Keystone).
+	 * @param region
+	 *            The particular OpenStack region (out of the ones available in
+	 *            Keystone's service catalog) to connect to. For example,
+	 *            {@code RegionOne}.
+	 */
+	public ApiAccessConfig(AuthConfig auth, String region) {
+		this(auth, region, DEFAULT_CONNECTION_TIMEOUT, DEFAULT_SOCKET_TIMEOUT);
+	}
+
+	/**
+	 * Creates a new {@link ApiAccessConfig}.
+	 *
+	 * @param auth
+	 *            Declares how to authenticate with the OpenStack identity
+	 *            service (Keystone).
+	 * @param region
+	 *            The particular OpenStack region (out of the ones available in
+	 *            Keystone's service catalog) to connect to. For example,
+	 *            {@code RegionOne}.
+	 * @param connectionTimeout
+	 *            The timeout in milliseconds until a connection is established.
+	 *            May be <code>null</code>. Default:
+	 *            {@value #DEFAULT_CONNECTION_TIMEOUT} ms.
+	 * @param socketTimeout
+	 *            The socket timeout ({@code SO_TIMEOUT}) in milliseconds, which
+	 *            is the timeout for waiting for data or, put differently, a
+	 *            maximum period inactivity between two consecutive data
+	 *            packets. May be <code>null</code>. Default:
+	 *            {@value #DEFAULT_SOCKET_TIMEOUT} ms.
+	 */
+	public ApiAccessConfig(AuthConfig auth, String region, Integer connectionTimeout, Integer socketTimeout) {
+		this.auth = auth;
+		this.region = region;
+		this.connectionTimeout = connectionTimeout;
+		this.socketTimeout = socketTimeout;
+		validate();
+	}
+
+	/**
+	 * Returns a description of how to authenticate with the OpenStack identity
+	 * service (Keystone).
+	 *
+	 * @return
+	 */
+	public AuthConfig getAuth() {
+		return this.auth;
+	}
+
+	/**
+	 * Returns the particular OpenStack region (out of the ones available in
+	 * Keystone's service catalog) to connect to. For example, {@code RegionOne}
+	 * .
+	 *
+	 * @return
+	 */
+	public String getRegion() {
+		return this.region;
+	}
+
+	/**
+	 * The timeout in milliseconds until a connection is established.
+	 *
+	 * @return
+	 */
+	public Integer getConnectionTimeout() {
+		return Optional.fromNullable(this.connectionTimeout).or(DEFAULT_CONNECTION_TIMEOUT);
+	}
+
+	/**
+	 * The socket timeout ({@code SO_TIMEOUT}) in milliseconds, which is the
+	 * timeout for waiting for data or, put differently, a maximum period
+	 * inactivity between two consecutive data packets.
+	 *
+	 * @return
+	 */
+	public Integer getSocketTimeout() {
+		return Optional.fromNullable(this.socketTimeout).or(DEFAULT_SOCKET_TIMEOUT);
+	}
+
+	/**
+	 * Performs basic validation of this configuration. Throws an
+	 * {@link IllegalArgumentException} on failure to validate the
+	 * configuration.
+	 *
+	 * @throws IllegalArgumentException
+	 */
+	public void validate() throws IllegalArgumentException {
+		checkArgument(this.auth != null, "no auth method specified");
+		checkArgument(this.region != null, "missing region");
+		this.auth.validate();
+		checkArgument(getConnectionTimeout() > 0, "connectionTimeout must be positive");
+		checkArgument(getSocketTimeout() > 0, "socketTimeout must be positive");
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(this.auth, this.region, getConnectionTimeout(), getSocketTimeout());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof ApiAccessConfig) {
+			ApiAccessConfig that = (ApiAccessConfig) obj;
+			return equal(this.auth, that.auth) && equal(this.region, that.region)
+					&& equal(getConnectionTimeout(), that.getConnectionTimeout())
+					&& equal(getSocketTimeout(), that.getSocketTimeout());
+		}
+		return false;
+	}
+
+}
---
+++ openstack/src/test/java/com/elastisys/scale/commons/openstack/TestOSClientFactory.java
@@ -0,0 +1,75 @@
+package com.elastisys.scale.commons.openstack;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.Test;
+import org.openstack4j.api.OSClient;
+
+import com.elastisys.scale.commons.json.JsonUtils;
+import com.elastisys.scale.commons.openstack.OSClientFactory.OSClientCreator;
+
+/**
+ * Verifies that the {@link OSClientFactory} creates the right kind of
+ * {@link OSClient} depending on the given {@link AuthConfig}.
+ */
+public class TestOSClientFactory {
+
+	private OSClientCreator creatorMock = mock(OSClientFactory.OSClientCreator.class);
+
+	/**
+	 * Verify that a driver config that specifies version 2 authentication is
+	 * created properly by the {@link OSClientFactory}.
+	 */
+	@Test
+	public void acquireClientFromV2Auth() {
+		OSClientFactory factory = new OSClientFactory(loadConfig("config/openstack-pool-config-authv2.json"),
+				this.creatorMock);
+		factory.acquireAuthenticatedClient();
+
+		verify(this.creatorMock).fromV2Auth("http://nova.host.com:5000/v2.0", "tenant", "clouduser", "cloudpass");
+		verifyNoMoreInteractions(this.creatorMock);
+	}
+
+	/**
+	 * Verify that a driver config that specifies domain-scoped version 3
+	 * authentication is created properly by the {@link OSClientFactory}.
+	 */
+	@Test
+	public void acquireClientFromDomainScopedV3Auth() {
+		OSClientFactory factory = new OSClientFactory(
+				loadConfig("config/openstack-pool-config-authv3-domain-scoped.json"), this.creatorMock);
+		factory.acquireAuthenticatedClient();
+
+		verify(this.creatorMock).fromDomainScopedV3Auth("http://nova.host.com:5000/v3", "domain_id", "user_id",
+				"secret");
+		verifyNoMoreInteractions(this.creatorMock);
+	}
+
+	/**
+	 * Verify that a driver config that specifies project-scoped version 3
+	 * authentication is created properly by the {@link OSClientFactory}.
+	 */
+	@Test
+	public void acquireClientFromProjectScopedV3Auth() {
+		OSClientFactory factory = new OSClientFactory(
+				loadConfig("config/openstack-pool-config-authv3-project-scoped.json"), this.creatorMock);
+		factory.acquireAuthenticatedClient();
+
+		verify(this.creatorMock).fromProjectScopedV3Auth("http://nova.host.com:5000/v3", "project_id", "user_id",
+				"secret");
+		verifyNoMoreInteractions(this.creatorMock);
+	}
+
+	/**
+	 * Loads the {@link OpenStackPoolDriverConfig} part of a
+	 * {@link BaseCloudPoolConfig}.
+	 *
+	 * @param cloudPoolConfigPath
+	 * @return
+	 */
+	private ApiAccessConfig loadConfig(String cloudPoolConfigPath) {
+		return JsonUtils.toObject(JsonUtils.parseJsonResource(cloudPoolConfigPath), ApiAccessConfig.class);
+	}
+}
